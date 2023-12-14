a2775b0917a2aedbb6762d4ee52882865cfe4871
joindevopscloud <info@joindevops.com>
2023-12-14 12:36:40 +0530

    roboshop shell scripts

--- 14-improve.sh
+++ 14-improve.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-set -e
+# set -e
 
-catch_errors() {
-    ret=$?
-    echo "Error occurred in script at line $1, message: $2"
-    exit $ret
-}
+# catch_errors() {
+#     ret=$?
+#     echo "Error occurred in script at line $1, message: $2"
+#     exit $ret
+# }
 
-# Trap errors and call the function with the line number
-trap 'catch_errors $LINENO' ERR
+# # Trap errors and call the function with the line number
+# trap 'catch_errors $LINENO' ERR
 
 ID=$(id -u)
 R="\e[31m"
@@ -22,14 +22,14 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
-# VALIDATE(){
-#     if [ $1 -ne 0 ]
-#     then
-#         echo -e "$2 ... $R FAILED $N"
-#     else
-#         echo -e "$2 ... $G SUCCESS $N"
-#     fi
-# }
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
 
 if [ $ID -ne 0 ]
 then
@@ -46,7 +46,7 @@ do
     then
         yum install $package -y &>> $LOGFILE
         echo "$package installation ... $G SUCCESS $N"
-        #VALIDATE $? "Installation of $package" # validate
+        VALIDATE $? "Installation of $package" # validate
     else
         echo -e "$package is already installed ... $Y SKIPPING $N"
     fi
