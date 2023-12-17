bd74d9ce2d4df831b5bafe7942497d51f7a57c8c
joindevopscloud <info@joindevops.com>
2023-12-17 22:08:04 +0530

    daws-76s

--- mail.sh
+++ mail.sh
@@ -5,9 +5,10 @@ ALERT_TYPE=$2
 #BODY=$(sed -e 's/$/\\n/g' <<< $3)
 BODY=$3
 echo "Body: $BODY"
+ESCAPED_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g')
 SUBJECT=$4
 TO_ADDRESS=$5
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPED_BODY/g" template.html)
 
 echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
