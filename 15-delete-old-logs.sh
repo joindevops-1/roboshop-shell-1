cbe18afda254cbc8f4013a96b37579eef2506fca
joindevopscloud <info@joindevops.com>
2023-12-17 11:48:46 +0530

    daws-76s

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -4,9 +4,9 @@ SOURCE="/tmp/shell-logs"
 
 FILES_TO_DELETE=$(find $SOURCE -type f -name "*.log" -mtime +14)
 
-echo "Files are: $FILES_TO_DELETE"
+#echo "Files are: $FILES_TO_DELETE"
 
 while IFS= read -r line; do
     # Process each line (for example, print it)
-    printf "Line: %s\n" "$line"
+    echo "$line"
 done <<< "$FILES_TO_DELETE"
