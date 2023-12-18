bae8fa17badcc528eb885c46b0c68cdb84754ccb
joindevopscloud <info@joindevops.com>
2023-12-18 21:26:29 +0530

    daws-76s

--- 19-greeting.sh
+++ 19-greeting.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-PERSON=$1
+usage() {
+  echo "Usage: $(basename "$0") -n <name> -w <wishes>"
+  echo "Options:"
+  echo "  -n, --name <name>      Specify the name (mandatory)"
+  echo "  -w, --wishes <wishes>  Specify the wishes (mandatory)"
+  echo "  -h, --help             Display this help and exit"
+}
+NAME=""
+WISHES=""
+while getopts ":n:w:h" opt; do
+  case $opt in
+    n) NAME="$OPTARG";;
+    w) WISHES="$OPTARG";;
+    h|\?) usage; exit;;
+  esac
+done
 
-echo "Hello $1, Good Evening. I am learning Shell script"
+if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
+  echo "Error: Both -n (name) and -w (wishes) are mandatory options."
+  usage
+  exit 1
+fi
+
+echo "Hello $NAME, $WISHES. I am learning Shell script"
