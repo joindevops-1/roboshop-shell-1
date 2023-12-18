29bbbdca586052a9abd3fcbc5116bdbd0d2c7491
joindevopscloud <info@joindevops.com>
2023-12-18 17:56:11 +0530

    daws-76s

--- roboshop.sh
+++ roboshop.sh
@@ -15,5 +15,6 @@ do
     then
         INSTANCE_TYPE="t3.small"
     fi
-    aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].[PrivateIpAddress]')
+    echo "Instance: $IP_ADDRESS"
 done
