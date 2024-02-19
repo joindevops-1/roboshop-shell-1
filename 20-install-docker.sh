a10cd9d3e3a69ea421a4610251617ede908ef386
joindevopscloud <info@joindevops.com>
2024-02-19 20:02:41 +0530

    jenkins

--- 20-install-docker.sh
+++ 20-install-docker.sh
@@ -38,7 +38,7 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 
 VALIDATE $? "Added docker repo"
 
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 VALIDATE $? "Installed docker components"
 
